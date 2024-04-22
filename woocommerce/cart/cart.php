a464e2ee9331d44dedf92f0acc9e8f62307741d3
Dean Pifar <private@deanpifar.com>
2024-04-22 15:19:48 +0100

    cart.php cleanup, added support for variable products and created file cor checkout

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -21,183 +21,189 @@ defined( 'ABSPATH' ) || exit;
 
 <?php do_action( 'woocommerce_before_cart' ); ?>
 
-
 <div class="sleekyCartTable">
   <div class="container">
-  <form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
-    <table class="sleekyCartTable__table">
-      <thead>
-        <tr>
-          <td><p>Your cart<span><?php echo WC()->cart->get_cart_contents_count(); ?> items</span></p></td>
-          <td>Quantity</td>
-          <td>Price</td>
-          <td>Subtotal</td>
-        </tr>
-      </thead>
-      <tbody>
-
-      <?php do_action( 'woocommerce_before_cart_contents' ); ?>
-      <?php foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) { ?>
-        <?php $_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key ); ?>
-        <?php $product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key ); ?>
-        <?php $product_name = apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ); ?>
+    <form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
       
+    <?php do_action( 'woocommerce_before_cart_table' ); ?>
+    
+      <table class="sleekyCartTable__table">
+
+        <thead>
+          <tr>
+            <td>
+              <p>Your cart<span><?php echo WC()->cart->get_cart_contents_count(); ?> items</span></p>
+            </td>
+            <td>Quantity</td>
+            <td>Price</td>
+            <td>Subtotal</td>
+          </tr>
+        </thead>
+
+        <tbody>
+
+        <?php do_action( 'woocommerce_before_cart_contents' ); ?>
+
+        <?php foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) { ?>
+
+        <?php $_product     = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key ); ?>
+        <?php $product_id   = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key ); ?>
+        <?php $product_name = apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ); ?>
+        
         <?php if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) { ?>
+
           <?php $product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key ); ?>
 
           <tr class="sleekyCartTable__item woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
           
-          <td class="product-information sleekyCartTable__information">
-            <div class="sleekyCartTable__information__thumbnail">
-            <?php
-						$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+            <td class="product-information sleekyCartTable__information">
 
-						if ( ! $product_permalink ) {
-							echo $thumbnail; // PHPCS: XSS ok.
-						} else {
-							printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
-						}
-						?>
-            </div>
-            <div class="sleekyCartTable__information__description">
-              <h2>
+              <div class="sleekyCartTable__information__thumbnail">
               <?php
-              if ( ! $product_permalink ) {
-                echo wp_kses_post( $product_name . '&nbsp;' );
-              } else {
-                /**
-                * This filter is documented above.
-                *
-                * @since 2.1.0
-                */
-                echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
-              }
+              $thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+              if ( ! $product_permalink ) { echo $thumbnail; } else { printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); }
               ?>
-              </h2>
-              <p><?php echo $_product->get_description(); ?></p>
-            </div>
-          </td>
+              </div>
 
-          <td class="product-quantity sleekyCartTable__quantity">
-            <p>Quantity</p>
-            <div class="sleekyCartTable__quantity__container"> 
-          <?php
-						if ( $_product->is_sold_individually() ) {
-							$min_quantity = 0;
-							$max_quantity = 1;
-						} else {
-							$min_quantity = 0;
-							$max_quantity = $_product->get_max_purchase_quantity();
-						}
+              <div class="sleekyCartTable__information__description">
+                <h2><?php if ( ! $product_permalink ) { echo wp_kses_post( $product_name . '&nbsp;' ); } else { echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) ); } ?></h2>
+                <?php if( $_product->get_description() ) { ?>
+                  <p><?php echo $_product->get_description(); ?></p>
+                <?php } ?>
 
-						$product_quantity = woocommerce_quantity_input(
-							array(
-								'input_name'   => "cart[{$cart_item_key}][qty]",
-								'input_value'  => $cart_item['quantity'],
-								'max_value'    => $max_quantity,
-								'min_value'    => $min_quantity,
-								'product_name' => $product_name,
-							),
-							$_product,
-							false
-						);
-            ?>
-            
-            <script>
-              function decreaseQuantity(element) {
-                event.preventDefault();
+                <?php do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key ); ?>
 
-                let currentValue = $(element).siblings('.quantity').find('.input-text.qty.text').val();
+                <?php echo wc_get_formatted_cart_item_data( $cart_item ); ?>
 
-                currentValue--;
-
-                $(element).siblings('.quantity').find('.input-text.qty.text').val(currentValue);
-                $(element).siblings('.quantity').find('.input-text.qty.text').attr('value', currentValue);
-
-                $(element).siblings('.quantity').find('.input-text.qty.text').trigger("change");
-              }
-
-              function increaseQuantity(element) {
-                event.preventDefault();
-
-                let disabled = $(element).hasClass('disable');
-                let currentValue = $(element).siblings('.quantity').find('.input-text.qty.text').val();
-
-                if (!disabled) {
-                  currentValue++;
-
-                  $(element).siblings('.quantity').find('.input-text.qty.text').val(currentValue);
-                  $(element).siblings('.quantity').find('.input-text.qty.text').attr('value', currentValue);
-
-                  $(element).siblings('.quantity').find('.input-text.qty.text').trigger("change");
+                <?php
+                if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
+                  echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
                 }
-              }
-            </script>
+                ?>
+              </div>
 
-            <button class="sleekyCartTable__quantity__decrease" onClick="decreaseQuantity(this);">-</button><?php
-						echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
-					?><button class="sleekyCartTable__quantity__increase <?php if ( $_product->is_sold_individually() ) { echo "disable"; } ?>" onClick="increaseQuantity(this);">+</button>
-        </div>  
-        </td>
+            </td>
 
-          <td class="product-price sleekyCartTable__price">
-          <?php
-						echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
-					?>
-          </td>
+            <td class="product-quantity sleekyCartTable__quantity">
+              <p>Quantity</p>
+              
+              <div class="sleekyCartTable__quantity__container"> 
+              <?php
+                if ( $_product->is_sold_individually() ) {
+                  $min_quantity = 0;
+                  $max_quantity = 1;
+                } else {
+                  $min_quantity = 0;
+                  $max_quantity = $_product->get_max_purchase_quantity();
+                }
 
-          <td class="product-subtotal sleekyCartTable__subtotal">
-          <?php
-						echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
-          ?>
-          <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+                $product_quantity = woocommerce_quantity_input(
+                  array(
+                    'input_name'   => "cart[{$cart_item_key}][qty]",
+                    'input_value'  => $cart_item['quantity'],
+                    'max_value'    => $max_quantity,
+                    'min_value'    => $min_quantity,
+                    'product_name' => $product_name,
+                  ),
+                  $_product,
+                  false
+                );
+              ?>   
+                <script>
+                  function decreaseQuantity(element) {
+                    event.preventDefault();
 
-            <div>
-            <?php
-								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-									'woocommerce_cart_item_remove_link',
-									sprintf(
-										'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
-										esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-										/* translators: %s is the product name */
-										esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
-										esc_attr( $product_id ),
-										esc_attr( $_product->get_sku() )
-									),
-									$cart_item_key
-								);
-							?>
-            </div>
-          </td>
-      
-        </tr>
+                    let currentValue = $(element).siblings('.quantity').find('.input-text.qty.text').val();
 
+                    currentValue--;
+
+                    $(element).siblings('.quantity').find('.input-text.qty.text').val(currentValue);
+                    $(element).siblings('.quantity').find('.input-text.qty.text').attr('value', currentValue);
+
+                    $(element).siblings('.quantity').find('.input-text.qty.text').trigger("change");
+                  }
+
+                  function increaseQuantity(element) {
+                    event.preventDefault();
+
+                    let disabled = $(element).hasClass('disable');
+                    let currentValue = $(element).siblings('.quantity').find('.input-text.qty.text').val();
+
+                    if (!disabled) {
+                      currentValue++;
+
+                      $(element).siblings('.quantity').find('.input-text.qty.text').val(currentValue);
+                      $(element).siblings('.quantity').find('.input-text.qty.text').attr('value', currentValue);
+
+                      $(element).siblings('.quantity').find('.input-text.qty.text').trigger("change");
+                    }
+                  }
+                </script>
+
+                <button class="sleekyCartTable__quantity__decrease" onClick="decreaseQuantity(this);">-</button>
+                <?php echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok. ?>
+                <button class="sleekyCartTable__quantity__increase <?php if ( $_product->is_sold_individually() ) { echo "disable"; } ?>" onClick="increaseQuantity(this);">+</button>
+              </div>  
+            </td>
+
+            <td class="product-price sleekyCartTable__price">
+            <?php echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok. ?>
+            </td>
+
+            <td class="product-subtotal sleekyCartTable__subtotal">
+            <?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok. ?>
+            
+            <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+
+              <div>
+              <?php
+                echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                  'woocommerce_cart_item_remove_link',
+                  sprintf(
+                    '<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+                    esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+                    /* translators: %s is the product name */
+                    esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
+                    esc_attr( $product_id ),
+                    esc_attr( $_product->get_sku() )
+                  ),
+                  $cart_item_key
+                );
+              ?>
+              </div>
+            </td>
+          </tr>
         <?php } ?>
-      <?php } ?>
+        <?php } ?>
+        <?php do_action( 'woocommerce_cart_contents' ); ?>
       </tbody>
     </table>
+
+    <?php do_action( 'woocommerce_after_cart_table' ); ?>
+
     <div class="sleekyCartTable__finish">
-				<div class="actions sleekyCartTable__coupon">
+			<div class="actions sleekyCartTable__coupon">
 
-					<?php if ( wc_coupons_enabled() ) { ?>
-						<div class="coupon">
-							<label for="coupon_code" class="screen-reader-text"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply', 'woocommerce' ); ?></button>
-							<?php do_action( 'woocommerce_cart_coupon' ); ?>
-						</div>
-					<?php } ?>
-
-					<button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
-
-					<?php do_action( 'woocommerce_cart_actions' ); ?>
-
-					<?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
+        <?php if ( wc_coupons_enabled() ) { ?>
+          <div class="coupon">
+            <label for="coupon_code" class="screen-reader-text"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply', 'woocommerce' ); ?></button>
+            <?php do_action( 'woocommerce_cart_coupon' ); ?>
           </div>
-        <div class="sleekyCartTable__totals">
-          <?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
-          <?php do_action( 'woocommerce_cart_collaterals' ); ?>
-        </div>
+        <?php } ?>
+
+        <button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
+
+        <?php do_action( 'woocommerce_cart_actions' ); ?>
+
+        <?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
+      </div>
+      <div class="sleekyCartTable__totals">
+        <?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
+        <?php do_action( 'woocommerce_cart_collaterals' ); ?>
+      </div>
     </div>
   </form>
+
   <div class="sleekyCartTable__continue">
     <div>
       <a class="back-to-shop" href="<?php echo get_permalink( wc_get_page_id( 'shop' ) ); ?>">Continue Shopping</a>
